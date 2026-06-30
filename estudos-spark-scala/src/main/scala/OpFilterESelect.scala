2722872b219185337374090fb4b16680be5fda2f
Luis Jackson <luisjacksonjr@gmail.com>
2026-06-30 13:40:42 -0300

    add exercicios filter/select

---
+++ estudos-spark-scala/src/main/scala/OpFilterESelect.scala
@@ -0,0 +1,49 @@
+//O Cenário: O time de RH precisa de uma lista rápida para enviar os parabéns para a galera mais experiente da empresa.
+//Sua Missão: Crie um novo DataFrame que contenha apenas os funcionários com mais de 50 anos. No resultado final, exiba apenas as colunas nome e idade.
+
+//Exercício 2: O "Corte de Gastos"
+//O Cenário: A diretoria está avaliando a folha de pagamento e quer mapear quem são os funcionários que não pertencem ao departamento de TI,
+//mas que têm salários de nível sênior.
+//Sua Missão: Filtre a base para encontrar todos os funcionários cujo departamento seja diferente de "TI"
+//E que ganhem um salario maior ou igual a 8000.0. Mostre na tela as colunas nome, departamento e salario.
+//
+//
+//Exercício 3:
+//O Cenário: Imagine que a empresa está fazendo uma análise de fraude ou auditoria interna.
+//Eles precisam cruzar dados de uma lista específica de departamentos suspeitos ou prioritários, focando em um público jovem.
+//Sua Missão: Filtre a tabela para trazer apenas os funcionários que trabalham no departamento de "Vendas" OR "RH"
+//(pode usar o operador ||), e que tenham menos de 30 anos. Para o relatório final ficar limpo, exiba todas as colunas menos a coluna id.
+
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+
+object OpFilterESelect {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("OpFilterESelect")
+      .master("local[*]")
+      .getOrCreate()
+
+    val df = spark.read
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .csv("dados/entrada/funcionarios_10k.csv")
+
+    val dfCinquentao = df.filter(col("idade") > 50)
+    dfCinquentao.select(col("nome"), col("idade")).show()
+
+    val funcionariosForaDoTI = df.filter(col("departamento") =!= "TI" && col("salario") >= 8000)
+    funcionariosForaDoTI.select(col("nome"), col("departamento"), col("salario")).show()
+
+    val funcVendasERh = df.filter((col("departamento") === "Vendas" || col("departamento") === "RH") && col("idade") < 30)
+
+    funcVendasERh.drop(col("id")).show()
+
+
+    spark.stop()
+
+  }
+}
--- estudos-spark-scala/src/main/scala/OperacoesSpark.scala
+++ estudos-spark-scala/src/main/scala/OperacoesSpark.scala
@@ -1,4 +1,5 @@
-import org.apache.spark.sql._
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.col
 
 object OperacoesSpark {
 
@@ -14,10 +15,16 @@ object OperacoesSpark {
 
     val df = spark.read
       .option("header", "true")
+      .option("inferSchema", "true")
       .csv("dados/entrada/funcionarios_10k.csv")
 
-    df.printSchema()
-    
+    val colunaID = df.select(col("id"), col("nome"), col("departamento"))
+
+    val colunaIdPares = df.filter(col("departamento") === "TI" && col("salario") > 10000 && col("id") % 2 === 0)
+
+    colunaIdPares.select(col("id"), col("nome"), col("salario")).show()
+
+      df.printSchema()
     spark.stop()
   }
 
